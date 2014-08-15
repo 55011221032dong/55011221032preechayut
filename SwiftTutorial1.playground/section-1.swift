d9633e3cf420b24606d5bb43009473a6e508daf1
55011221032dong <student@tests-iMac-5.local>
2014-08-15 16:39:48 +0700

    เชค output ของตัวแปรต่างๆ

--- SwiftTutorial1.playground/section-1.swift
+++ SwiftTutorial1.playground/section-1.swift
@@ -1,2 +1,21 @@
-let tutorialTream = 56
-let gogogo = 78
+let tutorialTeam = 56
+let editorialTeam = 23
+let totalTeam = tutorialTeam + editorialTeam
+
+totalTeam += 1
+
+let tutorialTeamInferred = -19
+let tutorialTeamExplicit: Int = -19
+
+let priceInferred = -19.99
+let priceExplicit:Double = -19.99
+
+let priceFloatInferred = -19.99
+let priceFloatExplicit:Float = -19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
+

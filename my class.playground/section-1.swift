232f3bee9ed895f30423d93a629598289f9feda6
55011221032dong <student@tests-iMac-8.local>
2014-09-03 15:29:29 +0700

    `แก้ๆ

--- my class.playground/section-1.swift
+++ my class.playground/section-1.swift
@@ -5,9 +5,20 @@ class myarray{
     init(test:Int,fac:Double){
     self.test=test
     self.fac=fac
-      
+    }
+    
+    func factory()->Double{
+        var res:Double
+        for (var i=0;i<10;i++){
+            res=1*i ;
+        }
+    return res
     }
     
     
 }
+var tt=myarray(test: 5, fac: 10)
+
+tt.factory()
+
 

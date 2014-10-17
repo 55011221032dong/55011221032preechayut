a7c27039e3bbaeb67f0ad581e95af11572123615
55011212167Per <IMac@MSU>
2014-10-17 15:21:11 +0700

    17 ตค 57
    17 ตค 57

---
+++ segue/segue/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  segue
+//
+//  Created by student on 10/17/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var unknown: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

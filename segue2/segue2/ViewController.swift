0011a23c478a84dc034339bcfc2771a85ace7557
55011212167Per <IMac@MSU>
2014-10-31 16:37:23 +0700

    อัพเดท

---
+++ segue2/segue2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  segue2
+//
+//  Created by student on 10/31/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorLebel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ segue2/segue2/colorTwoViewController.swift
@@ -0,0 +1,32 @@
+//
+//  colorTwoViewController.swift
+//  segue2
+//
+//  Created by student on 10/31/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class colorTwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
